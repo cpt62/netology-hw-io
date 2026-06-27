442c6c862bfbbfcd75aa249066c43820974a5122
cpt62 <rgrturzn7316@gmail.com>
2026-06-27 20:40:55 +0300

    task2 completed

--- src/task2/GameProgress.java
+++ src/task2/GameProgress.java
@@ -3,6 +3,12 @@ package task2;
 import task1.GameStructure;
 
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 
 public class GameProgress implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -29,12 +35,57 @@ public class GameProgress implements Serializable {
                 '}';
     }
 
-    public void save() {
+    public void saveGame(String dirRoot) {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss-SSS"));
+        File saveGames = new File(dirRoot, GameStructure.SAVE.getPath());
         try (ObjectOutputStream oos = new ObjectOutputStream(
                 new FileOutputStream(
-                        new File(GameStructure.SAVE.getPath(), "save.dat")))) {
+                        new File(saveGames, "save_" + timestamp + ".dat")))) {
+            oos.writeObject(this);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public void savesToZip(String dirRoot) {
+        File saveDir = new File(dirRoot, GameStructure.SAVE.getPath());
+        File[] files = saveDir.listFiles();
+        File fileZip = new File(saveDir, "savesGame.zip");
+
+        if (files == null || files.length == 0) return;
+        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(fileZip))) {
+            ZipEntry zipEntry;
+            byte[] buffer = new byte[4096];
+            for (File file : files) {
+                if (file.isDirectory()) continue;
+                zipEntry = new ZipEntry(file.getName());
+                zip.putNextEntry(zipEntry);
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    int length;
+                    while ((length = fis.read(buffer)) != -1) {
+                        zip.write(buffer, 0, length);
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                zip.closeEntry();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        clearDat(dirRoot);
+
+    }
+
+    private void clearDat(String dirRoot) {
+        File file = new File(dirRoot, GameStructure.SAVE.getPath());
+        File[] files = file.listFiles();
+        if (files == null || files.length == 0) {
+            System.out.println("Сохранения отсутствуют");
+            return;
+        }
+        Arrays.stream(files)
+                .filter(f -> f.getName().endsWith(".dat"))
+                .forEach(File::delete);
     }
 }
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,11 +1,25 @@
 package task2;
 
+import task1.Game;
+
 public class Main {
     public static void main(String[] args) {
+        Game game = new Game("/home/cpt62/projects/netology-hw-io/Games");
+        game.createStructure();
+
         GameProgress progress1 = new GameProgress(100, 5, 1, 0.3);
         GameProgress progress2 = new GameProgress(4, 3, 18, 14.5);
         GameProgress progress3 = new GameProgress(22, 4, 29, 44.1);
 
-        progress1.save();
+        progress1.saveGame(game.getRootDir());
+        try {
+            Thread.sleep(100);
+            progress2.saveGame(game.getRootDir());
+            Thread.sleep(100);
+            progress3.saveGame(game.getRootDir());
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+        progress1.savesToZip(game.getRootDir());
     }
 }
